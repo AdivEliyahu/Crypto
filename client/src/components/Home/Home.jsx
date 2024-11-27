33858f7392a88b826cd0a5b0d697cd30b58f3218
AdivEliyahu <Adiveliyahu3@gmail.com>
2024-11-27 18:12:03 +0200

    Home page, style and more folder structure

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Home.css"; 
+import { useNavigate } from "react-router-dom";
+
+
+
+
+export default function Home() {
+
+    const nav = useNavigate()
+    
+    const handleClick = (navTo) => { 
+        console.log(navTo);
+        nav(navTo);
+    }
+
+
+
+
+
+  return (
+
+    <div className="container">
+
+        <div className="title"> CRYPTOGRAPHY </div>
+        <div className="button-container">
+            <div className="clickable-div option" onClick={() => handleClick('Isomorfphic Graph')}>
+                    Isomorphic Graph
+            </div>
+            <div className="clickable-div option" onClick={() => handleClick('Diffie Hellman & AES')}>
+                    Diffie Hellman & AES
+            </div>
+        
+        </div>
+
+    </div>
+
+  );
+}
