98ede51726e0f07c509455f1a1830fe50c90527d
AdivEliyahu <Adiveliyahu3@gmail.com>
2025-01-09 22:09:01 +0200

    db connection

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class citizen(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    center_id = models.IntegerField()
+
+class center(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+
+
